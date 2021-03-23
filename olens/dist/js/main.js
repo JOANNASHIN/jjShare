13f61360d1ae35c46b5cabc36606f92e65e41370
jw_lim28 <jwlim@forbiz.co.kr>
2021-03-23 18:42:47 +0900

    슬라이드 매끄럽게 움직이기, 함수정리 * 슬라이드가 실행되지 않을 때 개별적 active 붙이는 거 해야함 * 슬라이드 0 앞부분 loop 처리해야함 * return 값 활용하기

--- olens/dist/js/main.js
+++ olens/dist/js/main.js
@@ -57509,8 +57509,72 @@ __webpack_require__.r(__webpack_exports__);
 
 const olenzReview = () => {
     const $document = $(document);
+
+    //슬라이드 개수 확인
+    const isSliderStart = () => {
+        const $review = $(".js__review");
+        const $slide = $review.find(".swiper-slide");
+        const $slideBtn = $review.find(".js__slider__nav");  
+        // const _slideLength = ( $slide.length ); 
+        const _slideLength = ( $slide.length / 2 ) //임시 팝업창 열렸을 때 다시 파악
+        const _showLength = 6;
+
+        if ( _slideLength > _showLength ){
+            $slideBtn.addClass("show");
+            pageReviewSlider(); // 리뷰페이지 실행
+            ReviewPopOpen(true);
+        } 
+        else {
+            // value = false;
+            $slideBtn.removeClass("show");
+            ReviewPopOpen(false);
+        }
+    }
+
+
+    // 팝업창 열기
+    const ReviewPopOpen = (value) => {
+        $document.on("click", ".js__slide__layerOpen", function() {
+            $(".fb__reviewPop").addClass("show");
+            let _target = $(this).attr("data-index");
+
+            isSelectSliderStart() //선택리뷰 갯수 체크
+
+            if ( value == true ){
+                allReviewSlider(_target); //전체리뷰 슬라이더 실행
+            } 
+            else {
+                reviewPageNotFull(_target); 
+            }
+        })
+    }
+
+
+    //선택리뷰 갯수 체크
+    const isSelectSliderStart = () => {
+        const $selecttNav = $(".selectReview__nav");
+        const _selectLength = $(".selectReview__slide").length;
+        const _showLength = 1;
+
+        if ( _selectLength > _showLength ){
+            $selecttNav.addClass("show");
+            selectReviewSlider(); //선택리뷰 슬라이더 실행
+        } 
+        else {
+            $selecttNav.removeClass("show");
+        }
+    } 
+
+
+    // 선택한 슬라이드마다 수기로 active
+    const reviewPageNotFull = (_target) => {
+        console.log("되냐", _target) 
+        // $(`.allReview__slide:nth-child(${_target})`).addClass('show');
+        
+    }
+
     
-    // 리뷰페이지 슬라이드
+    // 리뷰페이지 슬라이더
     const pageReviewSlider = () => {
         new swiper__WEBPACK_IMPORTED_MODULE_0__.default( ".js__reviewPage__slider", {
             loop: true,
@@ -57522,87 +57586,34 @@ const olenzReview = () => {
         });
     }
 
-    // 팝업창 열기
-    const ReviewPopOpen = () => {
-        $document.on("click", ".js__slide__layerOpen", function() {
-            $(".fb__reviewPop").addClass("show");
-            ReviewPopStart();
-            let _target = $(this).attr("data-index");
-            allReviewStart(_target);
-        })
-    }
 
-
-    // 팝업창 닫기
-    const ReviewPopClose = () => {
-        $document.on("click", ".fb__reviewPop__close", function() {
-            $(".fb__reviewPop").removeClass("show");
-        })
-    }
-
-
-    // 팝업창 스크립트 실행 
-    const ReviewPopStart = () => {
-        selectReviewSlider(); //선택리뷰 슬라이드
-    }
-
-    
-    // 전체리뷰 슬라이드 갯수에 따른 실행 정의
-    const allReviewStart = (_target) => {
-        const $area = $(".js__allreview");
-        
-        const $slide = $area.find(".swiper-slide");
-        const $slideBtn = $area.find(".js__slider__nav");
-        const _slideLength = $slide.length;
-        const _showLength = 6;
-        
-        if( _slideLength > _showLength ){
-            $slideBtn.addClass("show");
-            allReviewSlider(_target, _showLength); //전체리뷰 슬라이드 실행
-        } else {
-            $slideBtn.removeClass("show");
-        }
-    }
-
-
-    // 전체리뷰 슬라이드
-    const allReviewSlider = (_target, _showLength) => {
-        new swiper__WEBPACK_IMPORTED_MODULE_0__.default( ".js__allreview .swiper-container", {
+    // 전체리뷰 슬라이더
+    const allReviewSlider = (_target) => {
+        new swiper__WEBPACK_IMPORTED_MODULE_0__.default( ".allReview__container", {
             loop: true,
-            // preventClicks: true,
-            // preventClicksPropagation: true,
-            slidesPerView: 6,
+            centeredSlides: true,
+            loopFillGroupWithBlank: true,
+            slidesPerColumnFill: "row",
+            slidesPerView: "auto",
             navigation: {
                 prevEl: '.allReview__button--prev',
                 nextEl: '.allReview__button--next',
             },
             slideToClickedSlide : true,
             on: {
-                // 페이지 레이아웃의 슬라이드 값과 같은 값의 팝업창 전체리뷰 슬라이드를!
+                // 페이지 슬라이더의 슬라이드 값과 팝업창 전체리뷰 슬라이더의 슬라이드 값을 같도록!
                 init() {
-                    this.slideTo(Number(_target) + Number(_showLength), 1, true)
+                    this.slideTo(Number(_target), 1, true)
+                    // console.log("고른거", _target);
+                    // console.log("나온거", this.realIndex);
                 },
-
-                // click(swiper, event) {
-                //     //그 인덱스로 가라
-                //     // this.slideTo()
-            
-                //     // console.log("this", this); //this
-                //     // console.log("swiper", swiper); //this
-                //     // console.log("swiper", event); //사용 가능한 event
-                //     console.log("clickedIndex", this.clickedIndex); //this의 realIndex 
-                //     // realIndex가 슬라이드 한번 할 때마다 바뀜 0 - 1 - 2 -3 - 4 - 5 - 6 - 0
-                //     console.log("activeIndex", this.activeIndex); 
-                //     // 6 - 7 - 8 - 9 - 10 - 11 - 12 - 13 - 7
-                // }
             }
         });
 
-        // window.test = test;
     }
     
     
-    //선택리뷰 슬라이드
+    //선택리뷰 슬라이더
     const selectReviewSlider = () => {
         new swiper__WEBPACK_IMPORTED_MODULE_0__.default('.selectReview__container', {
             pagination: {
@@ -57616,10 +57627,19 @@ const olenzReview = () => {
         });
     }
     
+
+    // 팝업창 닫기
+    const ReviewPopClose = () => {
+    $document.on("click", ".fb__reviewPop__close", function() {
+        $(".fb__reviewPop").removeClass("show");
+    })
+    }
+
+
     // 가장 먼저 실행되는 것
     const init = () => {
-        pageReviewSlider(); //리뷰페이지 슬라이드
-        ReviewPopOpen(); //팝업창 열기
+        isSliderStart(); //페이지, 전체리뷰 갯수 체크
+        // ReviewPopOpen(); //팝업창 열기
         ReviewPopClose(); //팝업창 닫기
     }
     
@@ -57628,24 +57648,6 @@ const olenzReview = () => {
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (olenzReview);
 
-
-
-// // active 위치값 고정일 때, 전체리뷰 active
-// const allReviewActive = () => {
-//     const _activePosition = $(".allReview__slide:nth-child(3)").position().left;
-
-//     // 슬라이드를 클릭하면 동작
-//     $document.on("click", ".allReview__slide", function() {
-//         const _thisPosition = $(this).position().left;
-//         const _thisMovePosition = -(_thisPosition) + _activePosition
-        
-//         $(".allReview__slide").removeClass("active");
-//         $(this).addClass("active");
-
-//         $(".allReview__slider").css("transform","translate("+ _thisMovePosition + "px)");
-//         $(".allReview__slider").css("transition" ,"transform 0.3s");
-//     })
-// }
 
 /***/ }),
 
