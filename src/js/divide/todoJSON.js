b5390ee541e15f3619f0e0e2479976672d419e0e
jieunshin <joannashin3@forbiz.co.kr>
2021-04-19 10:21:51 +0900

    소스정리

--- src/js/divide/todoJSON.js
+++ src/js/divide/todoJSON.js
@@ -4,6 +4,7 @@ const todoJSON = () => {
     const requestTodoList = () => {
         $.getJSON("./json/todolist.json", (data) => {
             const todo = data.todo;
+            
             if (!todo || !todo.length) return false;
 
             todo.forEach(list => drawAdd(list));
