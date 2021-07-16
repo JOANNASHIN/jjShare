77b50e42dd9b24f42bc97b40373a9cd96a837348
DESKTOP-SLFFGSS\frontend <jmju@forbiz.co.kr>
2021-07-16 17:06:54 +0900

    투두 완성

--- dist/js/main.js
+++ dist/js/main.js
@@ -58786,21 +58786,25 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => __WEBPACK_DEFAULT_EXPORT__
 /* harmony export */ });
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);
 /* provided dependency */ var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+
+
 const minjuTodo = () => {
     const $document = $(document);
 
     const todoApp = () => {
         // 날짜 변수 선언
         const _now = new Date();
-        const weekly = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+        const weekly = ["일", "월", "화", "수", "목", "금", "토"];
         const _getYear = _now.getFullYear();
         const _getMonth = _now.getMonth();
         const _getDate = _now.getDate();
         const _getDay = _now.getDay();
 
         // 오늘 날짜 구하기
-        const _today = `${_getYear}.${_getMonth+1}.${_getDate} ${weekly[_getDay]}`
+        const _today = `${_getYear}.${_getMonth+1}.${_getDate} ${weekly[_getDay]+"요일"}`
         $(".js__today").text(_today);
 
         // 리스트 빈값 선언
@@ -58808,10 +58812,17 @@ const minjuTodo = () => {
         
         // 리스트 추가
         $document.on("click", ".js__btnAdd", function() {
+
             // 입력된 인풋 밸류값
             const _inputValue = $("#js__insert").val();
-            // html에서 인풋 불러오기
+            // html에서 인풋 찾기
             const insertInput = document.getElementById("js__insert");
+
+            // 내용 빈값이면 얼럿
+            if ( !_inputValue ) {
+                alert("할 일을 입력해주세요.")
+                return false;
+            }
 
             // 리스트 생성
             const addList = `
@@ -58834,17 +58845,18 @@ const minjuTodo = () => {
                     </div>
                 </li>
             `
-            $(".js__list__box").prepend(addList); // 리스트 맨앞에 추가
+            $(".js__list__box").prepend(addList);
 
+            // 하나 입력 후 인풋 비워주기
             insertInput.value = null;
 
             // 리스트 전체 개수
             $(".js__allCnt").text($(".js__list__row").length);
             
-            // 리스트 진행 개수
+            // 리스트 진행중 개수
             $(".js__ing").text( ($(".js__list__row").length) - ($(".js__list__row.dim").length));
             
-            // 리스트 개수가 1이상 되면 전체삭제 버튼
+            // 리스트 개수가 1이상 되면 전체삭제 bt 활성화
             if ( $(".js__list__row").length >= 1 ) {
                 $(".js__btnAllDelete").addClass("show");
             }
@@ -58863,10 +58875,6 @@ const minjuTodo = () => {
             // 딤추가/ 버튼 활성화/비활
             if ( !($this.hasClass("dim")) ) {
                 $this.addClass("dim");
-
-                // attr()로 버튼의 disabled 사용여부 (disabled 활성화/비활성화)
-                // attr()은 HTML의 속성을 취급
-                // prop()은 javescript의 property를 취급하는 메소드, disabled의 상태값 확인
                 $(".js__btnEdit").attr("disabled", true);
             }
             else {
@@ -58883,21 +58891,21 @@ const minjuTodo = () => {
         });
 
         // 리스트 수정
-        $document.on("click", ".js__btnEdit", function(e) {
+        $document.on("click", ".js__btnEdit", function() {
             const $this = $(this);
-            e.stopPropagation();
+            const $contBox = $this.closest(".js__list__row").find(".cont-box"); // 컨텐츠 박스
+            const $editBox = $this.closest(".js__list__row").find(".edit-box"); // 수정 박스
+
+            const _listTextValue = $this.closest(".js__list__row").find(".cont").text(); // 텍스트값
 
             // 컨텐츠 박스 숨기기
-            $this.closest(".js__list__row").find(".cont-box").addClass("contHide");
+            $contBox.addClass("contHide");
 
             // 수정 리스트 보이기
-            $this.closest(".js__list__row").find(".edit-box").addClass("editShow");
+            $editBox.addClass("editShow");
 
             // 리스트 텍스트 가져오기
-            $this.closest(".cont-box").next(".edit-box").find("input").val($this.closest(".js__list__row").find(".cont").text());
-
-            console.log( $this.closest(".js__list__row").find(".cont").text() );
-            
+            $editBox.find("input").val(_listTextValue);
         });
 
         // 리스트 수정 후 완료
@@ -58905,18 +58913,24 @@ const minjuTodo = () => {
             e.stopPropagation();
 
             const $this = $(this);
-            const _editDoneValue = $this.parent().siblings().val();
+            const $contBox = $this.closest(".js__list__row").find(".cont-box");
+            const $editBox = $this.closest(".js__list__row").find(".edit-box");
+
+            const _editDoneValue = $this.parent().siblings().val(); // 수정 완료값
+
+            if ( !_editDoneValue ) {
+                alert("내용을 입력해주세요.")
+                return false;
+            }
 
             // 컨텐츠 박스 보이게
-            $this.closest(".js__list__row").find(".cont-box").removeClass("contHide");
-            // 수정 텍스트 다시 가져오기
-            $this.closest(".js__list__row").find(".cont-box").find(".cont").text(_editDoneValue);
+            $contBox.removeClass("contHide");
 
-            console.log(_editDoneValue)
+            // 수정 텍스트 다시 가져오기
+            $contBox.find(".cont").text(_editDoneValue);
 
             // 수정 리스트 숨기기
-            $this.closest(".js__list__row").find(".edit-box").removeClass("editShow");
-
+            $editBox.removeClass("editShow");
         });
 
         // 수정 취소
@@ -58924,10 +58938,13 @@ const minjuTodo = () => {
             e.stopPropagation();
 
             const $this = $(this);
+            const $contBox = $this.closest(".js__list__row").find(".cont-box");
+            const $editBox = $this.closest(".js__list__row").find(".edit-box");
+
             // 컨텐츠 박스 보이게
-            $this.closest(".js__list__row").find(".cont-box").removeClass("contHide");
+            $contBox.removeClass("contHide");
              // 수정 리스트 숨기기
-             $this.closest(".js__list__row").find(".edit-box").removeClass("editShow");
+            $editBox.removeClass("editShow");
         });
 
         // 리스트 삭제
@@ -58935,31 +58952,29 @@ const minjuTodo = () => {
             const $this = $(this); 
             const findList = $this.closest($(".js__list__row"));
 
-            findList.remove(); // 리스트를 찾아서 삭제
+            findList.remove();
 
             // 전체 개수
             $(".js__allCnt").text($(".js__list__row").length);
 
             // 리스트가 없으면
             if ( $(".js__list__row").length == 0 ) {
-                $(".js__btnAllDelete").removeClass("show"); // 버튼 삭제
-                $(".todo__situation__box").find(".cnt-value").text("0"); // 리셋
+                $(".js__btnAllDelete").removeClass("show");
+                $(".todo__situation__box").find(".cnt-value").text("0");
             }
-           
         });
 
         // 전체 삭제
         $document.on("click", ".js__btnAllDelete", function() {
 
-            $(".js__list__box").empty(); //remove()를 하면 list__box가 통으로 삭제
+            $(".js__list__box").empty();
 
-            $(".todo__situation__box").find(".cnt-value").text("0"); // 카운트 리셋
+            $(".todo__situation__box").find(".cnt-value").text("0");
 
              if ( $(".js__list__row").length == 0 ) {
                 $(".js__btnAllDelete").removeClass("show");
             }
         });
-
     }
 
     todoApp();
--- src/js/divide/pcMinjuTodo.js
+++ src/js/divide/pcMinjuTodo.js
@@ -1,17 +1,19 @@
+import _ from "lodash";
+
 const minjuTodo = () => {
     const $document = $(document);
 
     const todoApp = () => {
         // 날짜 변수 선언
         const _now = new Date();
-        const weekly = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+        const weekly = ["일", "월", "화", "수", "목", "금", "토"];
         const _getYear = _now.getFullYear();
         const _getMonth = _now.getMonth();
         const _getDate = _now.getDate();
         const _getDay = _now.getDay();
 
         // 오늘 날짜 구하기
-        const _today = `${_getYear}.${_getMonth+1}.${_getDate} ${weekly[_getDay]}`
+        const _today = `${_getYear}.${_getMonth+1}.${_getDate} ${weekly[_getDay]+"요일"}`
         $(".js__today").text(_today);
 
         // 리스트 빈값 선언
@@ -19,10 +21,17 @@ const minjuTodo = () => {
         
         // 리스트 추가
         $document.on("click", ".js__btnAdd", function() {
+
             // 입력된 인풋 밸류값
             const _inputValue = $("#js__insert").val();
-            // html에서 인풋 불러오기
+            // html에서 인풋 찾기
             const insertInput = document.getElementById("js__insert");
+
+            // 내용 빈값이면 얼럿
+            if ( !_inputValue ) {
+                alert("할 일을 입력해주세요.")
+                return false;
+            }
 
             // 리스트 생성
             const addList = `
@@ -45,17 +54,18 @@ const minjuTodo = () => {
                     </div>
                 </li>
             `
-            $(".js__list__box").prepend(addList); // 리스트 맨앞에 추가
+            $(".js__list__box").prepend(addList);
 
+            // 하나 입력 후 인풋 비워주기
             insertInput.value = null;
 
             // 리스트 전체 개수
             $(".js__allCnt").text($(".js__list__row").length);
             
-            // 리스트 진행 개수
+            // 리스트 진행중 개수
             $(".js__ing").text( ($(".js__list__row").length) - ($(".js__list__row.dim").length));
             
-            // 리스트 개수가 1이상 되면 전체삭제 버튼
+            // 리스트 개수가 1이상 되면 전체삭제 bt 활성화
             if ( $(".js__list__row").length >= 1 ) {
                 $(".js__btnAllDelete").addClass("show");
             }
@@ -74,10 +84,6 @@ const minjuTodo = () => {
             // 딤추가/ 버튼 활성화/비활
             if ( !($this.hasClass("dim")) ) {
                 $this.addClass("dim");
-
-                // attr()로 버튼의 disabled 사용여부 (disabled 활성화/비활성화)
-                // attr()은 HTML의 속성을 취급
-                // prop()은 javescript의 property를 취급하는 메소드, disabled의 상태값 확인
                 $(".js__btnEdit").attr("disabled", true);
             }
             else {
@@ -94,21 +100,21 @@ const minjuTodo = () => {
         });
 
         // 리스트 수정
-        $document.on("click", ".js__btnEdit", function(e) {
+        $document.on("click", ".js__btnEdit", function() {
             const $this = $(this);
-            e.stopPropagation();
+            const $contBox = $this.closest(".js__list__row").find(".cont-box"); // 컨텐츠 박스
+            const $editBox = $this.closest(".js__list__row").find(".edit-box"); // 수정 박스
+
+            const _listTextValue = $this.closest(".js__list__row").find(".cont").text(); // 텍스트값
 
             // 컨텐츠 박스 숨기기
-            $this.closest(".js__list__row").find(".cont-box").addClass("contHide");
+            $contBox.addClass("contHide");
 
             // 수정 리스트 보이기
-            $this.closest(".js__list__row").find(".edit-box").addClass("editShow");
+            $editBox.addClass("editShow");
 
             // 리스트 텍스트 가져오기
-            $this.closest(".cont-box").next(".edit-box").find("input").val($this.closest(".js__list__row").find(".cont").text());
-
-            console.log( $this.closest(".js__list__row").find(".cont").text() );
-            
+            $editBox.find("input").val(_listTextValue);
         });
 
         // 리스트 수정 후 완료
@@ -116,18 +122,24 @@ const minjuTodo = () => {
             e.stopPropagation();
 
             const $this = $(this);
-            const _editDoneValue = $this.parent().siblings().val();
+            const $contBox = $this.closest(".js__list__row").find(".cont-box");
+            const $editBox = $this.closest(".js__list__row").find(".edit-box");
+
+            const _editDoneValue = $this.parent().siblings().val(); // 수정 완료값
+
+            if ( !_editDoneValue ) {
+                alert("내용을 입력해주세요.")
+                return false;
+            }
 
             // 컨텐츠 박스 보이게
-            $this.closest(".js__list__row").find(".cont-box").removeClass("contHide");
-            // 수정 텍스트 다시 가져오기
-            $this.closest(".js__list__row").find(".cont-box").find(".cont").text(_editDoneValue);
+            $contBox.removeClass("contHide");
 
-            console.log(_editDoneValue)
+            // 수정 텍스트 다시 가져오기
+            $contBox.find(".cont").text(_editDoneValue);
 
             // 수정 리스트 숨기기
-            $this.closest(".js__list__row").find(".edit-box").removeClass("editShow");
-
+            $editBox.removeClass("editShow");
         });
 
         // 수정 취소
@@ -135,10 +147,13 @@ const minjuTodo = () => {
             e.stopPropagation();
 
             const $this = $(this);
+            const $contBox = $this.closest(".js__list__row").find(".cont-box");
+            const $editBox = $this.closest(".js__list__row").find(".edit-box");
+
             // 컨텐츠 박스 보이게
-            $this.closest(".js__list__row").find(".cont-box").removeClass("contHide");
+            $contBox.removeClass("contHide");
              // 수정 리스트 숨기기
-             $this.closest(".js__list__row").find(".edit-box").removeClass("editShow");
+            $editBox.removeClass("editShow");
         });
 
         // 리스트 삭제
@@ -146,31 +161,29 @@ const minjuTodo = () => {
             const $this = $(this); 
             const findList = $this.closest($(".js__list__row"));
 
-            findList.remove(); // 리스트를 찾아서 삭제
+            findList.remove();
 
             // 전체 개수
             $(".js__allCnt").text($(".js__list__row").length);
 
             // 리스트가 없으면
             if ( $(".js__list__row").length == 0 ) {
-                $(".js__btnAllDelete").removeClass("show"); // 버튼 삭제
-                $(".todo__situation__box").find(".cnt-value").text("0"); // 리셋
+                $(".js__btnAllDelete").removeClass("show");
+                $(".todo__situation__box").find(".cnt-value").text("0");
             }
-           
         });
 
         // 전체 삭제
         $document.on("click", ".js__btnAllDelete", function() {
 
-            $(".js__list__box").empty(); //remove()를 하면 list__box가 통으로 삭제
+            $(".js__list__box").empty();
 
-            $(".todo__situation__box").find(".cnt-value").text("0"); // 카운트 리셋
+            $(".todo__situation__box").find(".cnt-value").text("0");
 
              if ( $(".js__list__row").length == 0 ) {
                 $(".js__btnAllDelete").removeClass("show");
             }
         });
-
     }
 
     todoApp();
